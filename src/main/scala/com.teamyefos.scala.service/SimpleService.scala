3f3b2f9c1ab9ba6cea7b1c8ff07a41077b61cdae
Victor Fernandez <victor.fernandez@mendeley.com>
2013-02-15 14:55:09 +0000

    First commit

---
+++ src/main/scala/com.teamyefos.scala.service/SimpleService.scala
@@ -0,0 +1,1 @@
+package com.teamyefos.scala.service
---
+++ src/main/scala/com.teamyefos.scala.service/SimpleServiceConfiguration.scala
@@ -0,0 +1,13 @@
+package com.teamyefos.scala.service
+
+import com.yammer.dropwizard.config.Configuration
+
+/**
+ * User: Victor
+ * Date: 15/02/2013
+ * Time: 14:49
+ */
+class SimpleServiceConfiguration extends Configuration {
+
+
+}
